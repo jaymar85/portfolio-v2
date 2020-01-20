4bc831a4bf2e7f7defac9fb4b28987deae446ea2
Jeremy Mark <jermark851@gmail.com>
2020-01-19 23:18:40 -0600

    added text display for rocket icon

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,26 +5,48 @@ import {faTwitter, faLinkedinIn, faGithub} from '@fortawesome/free-brands-svg-ic
 import './footer.css'
 
 class Footer extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            idName: 'notDisplayText'
+        }
+    }
+    showText = () => {
+        this.setState({idName: 'displayText'})
+    }
+
+    hideText = () => {
+        this.setState({idName: 'notDisplayText'})
+    }
+
     render() {
-        
+
         return (
             <footer>
                 <div className='row'>
                     <div className='social-media'>
                         <a className='btn2' href="https://twitter.com/Jeremy66630195" target='_blank'>
-                            <FontAwesomeIcon className='icon' icon={faTwitter} size='2x'/>
+                            <FontAwesomeIcon className='icon' icon={faTwitter} />
                         </a>
                         <a className='btn2' href='https://github.com/jaymar85' target='_blank'>
-                            <FontAwesomeIcon className='icon' icon={faGithub} size='2x'/>
+                            <FontAwesomeIcon className='icon' icon={faGithub} />
                         </a>
                         <a className='btn2' href='https://www.linkedin.com/in/jeremymark/' target='_blank'>
-                            <FontAwesomeIcon className='icon' icon={faLinkedinIn} size='2x'/>
+                            <FontAwesomeIcon className='icon' icon={faLinkedinIn} />
                         </a>
                     </div>
                     <div className='page-scroll'>
-                        <a href='#'><img id="rocket" src={rocket} alt='rocket ship'/></a>
+                        <a href='#'>
+                        <img id="rocket" 
+                        src={rocket} 
+                        alt='rocket ship'
+                        onMouseOver={this.showText}
+                        onMouseOut = {this.hideText}
+                        />
+                        </a>
                         <div>
-                            <p id='to-top-copy'>Beam me up, Scotty!</p>
+                            <p id={this.state.idName}>Beam me up, Scotty!</p>
                         </div>
                     </div>
                     <p>&#169; Jeremy Mark 2020</p>
