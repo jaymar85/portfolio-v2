a93709a8da9c6ba77f8d22594768617af054084f
Jeremy Mark <jermark851@gmail.com>
2020-02-03 23:39:12 -0600

    added nav

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Jeremy Mark</h2>
+                <ul>
+                    <li>About</li>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Nav;
